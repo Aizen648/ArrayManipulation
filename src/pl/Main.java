1b0852adc4204ee69a83d6fb28addd95e2f3d05e
Aizen648 <you@example.com>
2023-02-18 14:57:56 +0100

    - Main

--- src/pl/Main.java
+++ src/pl/Main.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<List<Integer>> lists = new ArrayList<>(Arrays.asList(Arrays.asList(1, 5, 3), Arrays.asList(4, 8, 7), Arrays.asList(6, 9, 1)));
-        System.out.println(ArrayManipulation.run(5,lists));
+        System.out.println(ArrayManipulation.run(4,lists));
     }
 }
